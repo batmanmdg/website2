24fc3b69cb08ecabb10b97f5ed2388cd0b9a7021
batmanmdg <patrick.r.poirier@gmail.com>
2025-05-28 21:53:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ function blowOutCandle() {
   showFireworks();
 }
 
-function showFireworks() {
-  const fireworks = document.getElementById('fireworks');
-  fireworks.style.display = 'block';
-  fireworks.innerHTML = "<p style='color: white; font-size: 24px;'>✨ Fireworks! ✨</p>";
-
-  setTimeout(() => {
-    fireworks.style.display = 'none';
-    showMakeAWish();
-  }, 5000);
-}
-
 function showMakeAWish() {
   const message = document.getElementById('make-a-wish');
   message.style.display = 'block';
