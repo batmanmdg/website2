10206ae2cfc10f467fdf985f36856bf3b0f607d1
batmanmdg <patrick.r.poirier@gmail.com>
2025-05-28 21:59:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function showMakeAWish() {
   const message = document.getElementById('make-a-wish');
   message.style.display = 'block';
 }
+
+function createSparkle() {
+  const sparkle = document.createElement('div');
+  sparkle.classList.add('sparkle');
+  sparkle.style.top = Math.random() * window.innerHeight + 'px';
+  sparkle.style.left = Math.random() * window.innerWidth + 'px';
+  document.body.appendChild(sparkle);
+
+  setTimeout(() => {
+    sparkle.remove();
+  }, 1000);
+}
+
+// Create sparkles every 300ms
+setInterval(createSparkle, 300);
+
